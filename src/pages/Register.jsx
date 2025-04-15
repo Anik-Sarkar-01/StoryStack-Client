340ca59feafe2a77d9d06c5efb0d0027ec5eb843
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-04-15 20:33:14 +0600

    initial commit

---
+++ src/pages/Register.jsx
@@ -0,0 +1,37 @@
+import Lottie from 'lottie-react';
+import React from 'react';
+import registerLottie from "../assets/lottie/register-lottie.json"
+
+const Register = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col-reverse lg:flex-row-reverse">
+                <div className="text-center lg:text-left w-72">
+                   <Lottie animationData={registerLottie} loop={true}></Lottie> 
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className="card-body">
+                        <h1 className="text-5xl font-bold">Register now!</h1>
+                        <form onSubmit={handleSubmit} className="fieldset">
+                            <label className="fieldset-label">Email</label>
+                            <input type="email" className="input" name='email' placeholder="Email" />
+                            <label className="fieldset-label">Password</label>
+                            <input type="password" className="input" name='password' placeholder="Password" />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button className="btn btn-neutral mt-4">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
