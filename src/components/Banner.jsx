6a8cd66f34f9894dfe48c29ad039eda06e514249
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-04-19 13:39:21 +0600

    modified banner added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,23 +4,37 @@ import bannerImg2 from "../assets/bannerImg-02.jpg"
 import bannerImg3 from "../assets/bannerImg-03.jpg"
 // eslint-disable-next-line no-unused-vars
 import { motion } from "motion/react"
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { Link } from 'react-router-dom';
 
 
 const Banner = () => {
     return (
         <div className="hero bg-[#FEF9E1] min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse ]">
+            <div className="hero-content flex-col lg:flex-row-reverse gap-8">
                 <div className='flex-1'>
-                    <h1 className="text-5xl font-bold">Discover the life changing stories!</h1>
-                    <p className="py-6 text-justify">
+                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-relaxed">Discover the life changing stories !</h1>
+                    <p className="py-6 text-justify leading-relaxed">
                         Step into the lives of remarkable individuals and uncover powerful stories of growth, resilience, and transformation that will inspire you to see the world and yourself in a whole new light.
                     </p>
-                    <button className="btn bg-[#F98514] text-white rounded-none">Read Now</button>
+                    <Link to={'/all-blogs'}>
+                        <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className="btn bg-[#F98514] text-white rounded-none">
+                            Read Now
+                        </motion.button>
+                    </Link>
                 </div>
                 <div className='flex-1 flex gap-3'>
-                    <motion.img animate={{ y: [0, 25, 0] }} transition={{ duration: 10, repeat: Infinity }} src={bannerImg1} className="max-w-32 lg:max-w-48 object-cover h-96 shadow-2xl" />
-                    <motion.img animate={{ y: [0, -25, 0] }} transition={{ duration: 10, repeat: Infinity }} src={bannerImg2} className="max-w-32 lg:max-w-48 object-cover h-96 shadow-2xl" />
-                    <motion.img animate={{ y: [0, 25, 0] }} transition={{ duration: 10, repeat: Infinity }} src={bannerImg3} className="max-w-32 lg:max-w-48 object-cover h-96 shadow-2xl" />
+                    <PhotoProvider>
+                        <PhotoView src={bannerImg1}>
+                            <motion.img animate={{ y: [0, 20, 0] }} transition={{ duration: 8, repeat: Infinity }} src={bannerImg1} className="max-w-36 lg:max-w-56 object-cover h-96 shadow-2xl border-8 border-white" />
+                        </PhotoView>
+                        <PhotoView src={bannerImg2}>
+                            <motion.img animate={{ y: [0, -20, 0] }} transition={{ duration: 8, repeat: Infinity }} src={bannerImg2} className="max-w-32 lg:max-w-44 object-cover h-96 shadow-2xl border-4 border-white" />
+                        </PhotoView>
+                        <PhotoView src={bannerImg3}>
+                            <motion.img animate={{ y: [0, 20, 0] }} transition={{ duration: 8, repeat: Infinity }} src={bannerImg3} className="max-w-36 lg:max-w-56 object-cover h-96 shadow-2xl border-8 border-white" />
+                        </PhotoView>
+                    </PhotoProvider>
                 </div>
 
             </div>
